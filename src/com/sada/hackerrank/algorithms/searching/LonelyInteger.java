2d93bbf6a3ee7edcba55292c268f7fffd795608f
Sada Kurapati <sadakurapati@gmail.com>
2014-01-15 16:18:08 -0800

    Corrected package name.

--- src/com/sada/hackerrank/algorithms/searching/LonelyInteger.java
+++ src/com/sada/hackerrank/algorithms/searching/LonelyInteger.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package com.sada.hackerrannk.algorithms.searching;
+package com.sada.hackerrank.algorithms.searching;
 
 import java.util.Scanner;
 
